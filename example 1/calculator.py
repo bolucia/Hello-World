ba4e595301faa2b13fbcf68b779a4f11426cf844
bolucian <wambuilucian@gmail.com>
2019-04-04 21:57:17 +0300

    calculator.py

---
+++ example 1/calculator.py
@@ -0,0 +1,32 @@
+def add(a,b):
+	answer=a+b
+	return answer
+
+def subtract(b,c):
+	answer=b-c
+	return answer
+
+def divide(d,e):
+	answer=d/e
+	return answer
+
+def modulus(f,g):
+	answer=f%g
+	return answer
+
+def multiply(h,i):
+	answer=h*i
+	return answer
+
+def wholenumber(j,k):
+	answer=j//k
+	return answer
+
+def exponent(l,m):
+	answer=l**m
+	return answer
+
+def percentage(n,o):
+	answer=n%o
+	return answer
+
